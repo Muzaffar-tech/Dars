ef2f135974d7350cacce5941b01f5c35a4b7bc9b
Muzaffarjon <MuzaffarjonOmonov@mail.uz>
2024-07-17 20:18:35 +0500

    First commit for branch version2

--- Lesson3.py
+++ Lesson3.py
@@ -30,30 +30,37 @@
 # Db=db()
 # print(Db.connect())
 
-import psycopg2
+# import psycopg2
 
-def db():
-    database = 'fn22'
-    user = 'postgres'
-    password = 'fn2223'
-    host = 'localhost'
-    port = 5432
+# def db():
+#     database = 'fn22'
+#     user = 'postgres'
+#     password = 'fn2223'
+#     host = 'localhost'
+#     port = 5432
 
-    class Main:
-        def __init__(self):
-            self.conn = None
+#     class Main:
+#         def __init__(self):
+#             self.conn = None
 
-        def connect(self):
-            try:
-                self.conn = psycopg2.connect(dbname=database, user=user, password=password, host=host, port=port)
-                self.conn.autocommit = True
-                print("Connection established")
-            except Exception as e:
-                print(f"Unable to connect: {e}")
+#         def connect(self):
+#             try:
+#                 self.conn = psycopg2.connect(dbname=database, user=user, password=password, host=host, port=port)
+#                 self.conn.autocommit = True
+#                 print("Connection established")
+#             except Exception as e:
+#                 print(f"Unable to connect: {e}")
 
-    return Main()
+#     return Main()
 
-Db = db()
-Db.connect()
+# Db = db()
+# Db.connect()
+
+from dataclasses import dataclass
+
+@dataclass
+class User:
+    id:int
+    first_name: str
 
     
